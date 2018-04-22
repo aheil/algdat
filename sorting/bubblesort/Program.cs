6258ada36f4bb7998f292f3c5cbc432bd0136c19
Andreas Heil <info@aheil.de>
2018-04-22 23:13:41 +0200

    initial bubble sort added

---
+++ sorting/bubblesort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // init
+            var rnd = new Random(1337);
+            var items = new int[10];
+            for (int i = 0; i <= 9; i++)
+            {
+                items[i] = rnd.Next(100);
+            }
+
+            Write("Input:", items);
+
+           
+                //Write("Step: ", items);
+            
+
+            Write("Output:", items);
+        }
+
+        public static void Write(string label, int[] items)
+        {
+            System.Console.Write(label + "\t");
+            for (int i = 0; i < 9; i++)
+            {
+                System.Console.Write(items[i] + ", ");
+            }
+            System.Console.WriteLine(items[9]);
+        }
+    }
+}
