27c930e4e7c2c8834603e3d398903ab00f0d4f67
Andreas Heil <info@aheil.de>
2018-05-31 11:37:28 +0200

    empty expnential search projects imported

---
+++ searching/exponetialsearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exponetialsearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
