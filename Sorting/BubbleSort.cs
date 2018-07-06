d346b80144992f462e173dcac51e63c94cdfdbd6
Andreas Heil <info@aheil.de>
2018-07-07 01:47:42 +0200

    project structure refactoring

---
+++ Sorting/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace aheil.AlgDat.Sorting
+{
+    public class BubbleSort
+    {
+        public int[] Sort(int[] item)
+        {
+            bool switched = false;
+            do
+            {
+                switched = false;
+                for (int i = 0; i <= item.Length - 2; i++)
+                {
+                    if (item[i] > item[i + 1])
+                    {
+                        var temp = item[i + 1];
+                        item[i + 1] = item[i];
+                        item[i] = temp;
+                        switched = true;
+                    }
+                }
+            } while (switched);
+            return item;    
+        }
+    }
+}
---
+++ Sorting/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace aheil.AlgDat.Sorting
+{
+    public class SelectionSort
+    {
+        public int[] Sort(int[] item)
+        {
+            for (int i = 0; i < item.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < item.Length; j++)
+                {
+                    if (item[j] < item[min])
+                    {
+                        min = j;
+                    }
+                }
+                int tmp = item[min];
+                item[min] = item[i];
+                item[i] = tmp;
+            }
+            return item;
+        }
+    }
+}
